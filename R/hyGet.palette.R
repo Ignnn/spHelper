1eec6a7a4505b44f36ec1e130ae63d34125bd337
GegznaV <GegznaV@gmail.com>
2016-03-05 00:46:30 +0200

    v0.1.049

--- R/hyGet.palette.R
+++ R/hyGet.palette.R
@@ -10,16 +10,39 @@
 #' @export
 #'
 #' @examples
+#'
 #' data(Spectra)
 #' Spectra2 <- hyAdd.color(Spectra, "class")
 #'
+#' colnames(Spectra)
+#'    #> [1] "gr"    "class" "spc"
+#'
+#' colnames(Spectra2)
+#'    #> [1] "gr"     "class"  "spc"    ".color"
+#'
 #' # Names of colors, used for each level of factor variable
+#' labels(Spectra2,".color")
+#'    #>  "#377EB8" "#4DAF4A" "#984EA3" "#FF7F00"
 #'
 #' hyGet.palette(Spectra2)
 #'    #>  "#377EB8" "#4DAF4A" "#984EA3" "#FF7F00"
 #'
+#' # ATTENTION -------------------------------------------------
+#'
+#' # Preserve labels/ color palette:
+#' Spectra2[1,".color"] <- "red"
+#' hyGet.palette(Spectra2)
+#'     #>  "#377EB8" "#4DAF4A" "#984EA3" "#FF7F00"
+#'
+#' # Overwrite labels/ color palette:
+#' Spectra2$.color[1] <- "red"
+#' hyGet.palette(Spectra2)
+#'     #>  Warning message:
+#'     #>  In hyGet.palette(Spectra2) : Values of pallete do not exist.
+#'     #>  Most probably they are overwriten by operation `$.color<-`.
+#'
 #' labels(Spectra2,".color")
-#'    #>  "#377EB8" "#4DAF4A" "#984EA3" "#FF7F00"
+#'     #>  ".color"
 #'
 #' @family functions for \pkg{hyperSpec}
 #' @author Vilmantas Gegzna
@@ -27,8 +50,14 @@ hyGet.palette <- function(sp){
     if (class(sp) == "hyperSpec") {
         Palette <- labels(sp,".color")
         if (is.null(Palette)) Palette <- unique(sp$.color)
+        if (any(Palette == ".color")) {
+            warning(paste0("Values of pallete do not exist.\n",
+                          "Most probably they are overwriten by operation `$.color<-`."))
+            invisible(NULL)
+        } else {
         if (length(Palette) == 1) warning("Only 1 unique color was found!")
         return(Palette)
+        }
     } else {
         return(NULL)
     }
